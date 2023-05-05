4ab1d8b41a7fd1270c90fc97be54f939138af220
TymeOn <46976927+TymeOn@users.noreply.github.com>
2023-05-05 19:49:25 +0200

    feat: base for file upload

--- app/app/src/main/java/com/anmvg/celi/ListenerFragment.java
+++ app/app/src/main/java/com/anmvg/celi/ListenerFragment.java
@@ -17,8 +17,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -33,46 +31,84 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class ListenerFragment extends Fragment {
 
+    // Main binding
     private FragmentListenerBinding binding;
+
+    // Permissions to RECORD_AUDIO
+    static final int RecordAudioRequestCode = 1;
+    private static final String PERMISSION_TAG = "Permission TAG";
+    private static String fileName = null;
+    private final String[] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private final ActivityResultLauncher<Intent> storageActivityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(), result -> {
+            Log.d(PERMISSION_TAG, "onActivityResult :");
+
+            //Here we will handle the result of our intent
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                //Android is 11(R) or above
+                if (Environment.isExternalStorageManager()) {
+                    // Manage External Storage Permission is granted
+                    Log.d(PERMISSION_TAG, "onActivityResult : Manage External Storage Permission is granted");
+                    createFile();
+                } else {
+                    // Manage External Storage Permission is denied
+                    Log.d(PERMISSION_TAG, "onActivityResult : Manage External Storage Permission is denied");
+                }
+            }
+        }
+    );
+
+    // UI variables
     private boolean listening;
     private RippleBackground rippleBg;
 
-    private static final String PERMISSION_TAG = "Permission TAG";
+    // Media recorder
     private MediaRecorder recorder = null;
-    private static String fileName = null;
 
-    // Requesting permission to RECORD_AUDIO
-    static final int RecordAudioRequestCode = 1;
-    private final String [] permissions = { Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+    // HTTP requests client
+    private final OkHttpClient client = new OkHttpClient();
+
+
+    // BASIC VIEW FUNCTIONS
+    // --------------------
 
     @Override
     public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
+        @NonNull LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
     ) {
         binding = FragmentListenerBinding.inflate(inflater, container, false);
         fileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "AudioRecordCeli.mp3";
-
+        uploadVoiceFile();
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        if (checkPermissions())
-        {
+        // Check and request permissions
+        if (checkPermissions()) {
             Log.d(PERMISSION_TAG, "onCLick: Permission already granted ...");
             createFile();
-        }
-        else
-        {
+        } else {
             Log.d(PERMISSION_TAG, "onCLick: Permission was not granted, request ...");
             requestPermission();
         }
 
+        // Listen button behavior
         rippleBg = binding.rippleBg;
         binding.listenButton.setOnClickListener(view1 -> {
             listening = !listening;
@@ -86,116 +122,66 @@ public class ListenerFragment extends Fragment {
             }
 
             onRecord(listening);
+
+            if (!listening) {
+                // TODO VOSK CALL
+            }
 //            NavHostFragment.findNavController(ListenerFragment.this)
 //                    .navigate(R.id.action_ListenerFragment_to_PlayerFragment);
         });
     }
 
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
-    private void startRecording(){
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        recorder.setOutputFile(fileName);
 
-        try {
-            recorder.prepare();
-        } catch (IOException e) {
-           e.printStackTrace();
-        }
+    // PERMISSIONS FUNCTIONS
+    // ---------------------
 
-        recorder.start();
-    }
-    private void stopRecording() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
-    }
+    private boolean checkPermissions() {
+        int record = ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.RECORD_AUDIO);
 
-    private final ActivityResultLauncher<Intent> storageActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    Log.d(PERMISSION_TAG, "onActivityResult :");
-
-                    //Here we will handle the result of our intent
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-                        //Android is 11(R) or above
-                        if (Environment.isExternalStorageManager()){
-                            // Manage External Storage Permission is granted
-                            Log.d(PERMISSION_TAG, "onActivityResult : Manage External Storage Permission is granted");
-                            createFile();
-                        }
-                        else {
-                            // Manage External Storage Permission is denied
-                            Log.d(PERMISSION_TAG, "onActivityResult : Manage External Storage Permission is denied");
-                        }
-                    }
-                }
-            }
-
-    );
-
-    private boolean checkPermissions()
-    {
-        int record = ContextCompat.checkSelfPermission(Objects.requireNonNull(this.getContext()), Manifest.permission.RECORD_AUDIO);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-        {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             //Android is 11(R) or above
             return record == PackageManager.PERMISSION_GRANTED &&
                     Environment.isExternalStorageManager();
-        }
-        else
-        {
+        } else {
             //Android is below 11(R)
-            int write = ContextCompat.checkSelfPermission(Objects.requireNonNull(this.getContext()), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int write = ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
             return record == PackageManager.PERMISSION_GRANTED &&
                     write == PackageManager.PERMISSION_GRANTED;
         }
     }
 
-    public void requestPermission()
-    {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-        {
+    public void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             //Android is 11(R) or above
             try {
                 Log.d(PERMISSION_TAG, "requestPermission : try");
 
-                ActivityCompat.requestPermissions(Objects.requireNonNull(this.getActivity()), new String[] {Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+                ActivityCompat.requestPermissions(this.requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
 
                 Intent intent = new Intent();
                 intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                Uri uri = Uri.fromParts("package", Objects.requireNonNull(this.getActivity()).getPackageName(), null);
+                Uri uri = Uri.fromParts("package", this.requireActivity().getPackageName(), null);
                 intent.setData(uri);
                 storageActivityResultLauncher.launch(intent);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Log.d(PERMISSION_TAG, "requestPermission : catch", e);
 
-                ActivityCompat.requestPermissions(Objects.requireNonNull(this.getActivity()), new String[] {Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+                ActivityCompat.requestPermissions(this.requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
 
                 Intent intent = new Intent();
                 intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
                 storageActivityResultLauncher.launch(intent);
             }
-        }
-        else
-        {
+        } else {
             //Android is below 11(R)
-            ActivityCompat.requestPermissions(Objects.requireNonNull(this.getActivity()), permissions, RecordAudioRequestCode);
+            ActivityCompat.requestPermissions(this.requireActivity(), permissions, RecordAudioRequestCode);
         }
     }
 
@@ -219,10 +205,38 @@ public class ListenerFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+
+    // VOICE RECORDING
+    // ---------------
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setOutputFile(fileName);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
     }
 
     @Override
@@ -238,20 +252,14 @@ public class ListenerFragment extends Fragment {
         File file = new File(fileName);
 
         try {
-            if (file.exists())
-            {
-               Log.d("File", "File exists");
-            }
-            else
-            {
+            if (file.exists()) {
+                Log.d("File", "File exists");
+            } else {
                 boolean fileCreated = file.createNewFile();
 
-                if (fileCreated)
-                {
+                if (fileCreated) {
                     Log.d("File", "File created");
-                }
-                else
-                {
+                } else {
                     Log.d("File", "File not created");
                 }
             }
@@ -259,4 +267,38 @@ public class ListenerFragment extends Fragment {
             e.printStackTrace();
         }
     }
+
+
+    // ASR
+    // ---
+
+    void uploadVoiceFile() {
+        File file = new File(fileName);
+
+        RequestBody formBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart(
+                        "file",
+                        file.getName(),
+                        RequestBody.create(MediaType.parse("audio/mpeg"), file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://<IP>:5000/nlp")
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String responseText = Objects.requireNonNull(response.body()).string();
+                Log.d("ASR", responseText);
+            }
+        });
+    }
+
 }
